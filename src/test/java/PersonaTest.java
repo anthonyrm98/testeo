c9bcdc0c89406bf4598132c938f5232ee9a8a0fb
anthony <amanuelrm98@live.com>
2020-02-19 16:58:03 -0600

    Commit 2 Probando

--- src/test/java/PersonaTest.java
+++ src/test/java/PersonaTest.java
@@ -1,6 +1,8 @@
 public class PersonaTest {
     public static void main(String []arcs){
         Persona p1=new Persona("Anthony",111);
+        Persona p2=new Persona("Maluma",222);
         System.out.println("Datos: "+p1.toString());
+        System.out.println("Datos: "+p2.toString());
     }
 }
